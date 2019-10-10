b0c5cf23ebf8d1a888bdd103b6816b51456a5894
E <etornam.banini@cerner.com>
2019-10-10 09:43:26 -0400

    adding testing statements

--- dist/js/cerner-smart-embeddable-lib-1.2.0.js
+++ dist/js/cerner-smart-embeddable-lib-1.2.0.js
@@ -905,18 +905,21 @@ __webpack_require__(45);
 "use strict";
 
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; /* globals window */
+
 var _cernerSmartEmbeddableLib = __webpack_require__(46);
 
 var _cernerSmartEmbeddableLib2 = _interopRequireDefault(_cernerSmartEmbeddableLib);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-window.CernerSmartEmbeddableLib = window.CernerSmartEmbeddableLib || {}; /* globals window */
-
-if (window.CernerSmartEmbeddableLib.embedded) {
+window.CernerSmartEmbeddableLib = window.CernerSmartEmbeddableLib || {};
+console.log(_typeof('CCLINK') === (typeof Functions === 'undefined' ? 'undefined' : _typeof(Functions)));
+console.log(window);
+if (_typeof('CCLINK') === (typeof Functions === 'undefined' ? 'undefined' : _typeof(Functions))) {
   _cernerSmartEmbeddableLib2.default.init();
   _cernerSmartEmbeddableLib2.default.listenForCustomFrameHeight();
-
+  console.log('Hello Millenium User');
   window.CernerSmartEmbeddableLib.calcFrameHeight = _cernerSmartEmbeddableLib2.default.calcFrameHeight;
   window.CernerSmartEmbeddableLib.setFrameHeight = _cernerSmartEmbeddableLib2.default.setFrameHeight;
 }
--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,12 @@
 import CernerSmartEmbeddableLib from './cerner-smart-embeddable-lib';
 
 window.CernerSmartEmbeddableLib = window.CernerSmartEmbeddableLib || {};
-
-if (window.CernerSmartEmbeddableLib.embedded) {
+console.log(typeof 'CCLINK' === typeof Functions);
+console.log(window);
+if (typeof 'CCLINK' === typeof Functions) {
   CernerSmartEmbeddableLib.init();
   CernerSmartEmbeddableLib.listenForCustomFrameHeight();
-
+  console.log('Hello Millenium User');
   window.CernerSmartEmbeddableLib.calcFrameHeight = CernerSmartEmbeddableLib.calcFrameHeight;
   window.CernerSmartEmbeddableLib.setFrameHeight = CernerSmartEmbeddableLib.setFrameHeight;
 }
