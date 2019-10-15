b7b7a4e77f8c04f0b3a06ec393a606cefc6fd37c
E <etornam.banini@cerner.com>
2019-10-15 09:15:30 -0400

    testing new approach

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,10 @@
 import CernerSmartEmbeddableLib from './cerner-smart-embeddable-lib';
 
 window.CernerSmartEmbeddableLib = window.CernerSmartEmbeddableLib || {};
+if (window.gon.embedded) {
+  CernerSmartEmbeddableLib.init();
+  CernerSmartEmbeddableLib.listenForCustomFrameHeight();
 
-window.CernerSmartEmbeddableLib.embedded = typeof 'CCLLINK' === typeof Function;
-CernerSmartEmbeddableLib.init();
-CernerSmartEmbeddableLib.listenForCustomFrameHeight();
-
-window.CernerSmartEmbeddableLib.calcFrameHeight = CernerSmartEmbeddableLib.calcFrameHeight;
-window.CernerSmartEmbeddableLib.setFrameHeight = CernerSmartEmbeddableLib.setFrameHeight;
+  window.CernerSmartEmbeddableLib.calcFrameHeight = CernerSmartEmbeddableLib.calcFrameHeight;
+  window.CernerSmartEmbeddableLib.setFrameHeight = CernerSmartEmbeddableLib.setFrameHeight;
+}
