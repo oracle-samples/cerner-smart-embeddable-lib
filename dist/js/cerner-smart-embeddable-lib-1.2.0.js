56b541029a90087f0b759f2c8f6025eaf614dd94
E <etornam.banini@cerner.com>
2019-10-29 15:04:34 -0400

    Revert "removing localhost"
    This reverts commit 0c40e6415ab7696bf092dce591e4ef3e35b03a2a.

--- dist/js/cerner-smart-embeddable-lib-1.2.0.js
+++ dist/js/cerner-smart-embeddable-lib-1.2.0.js
@@ -911,16 +911,18 @@ var _cernerSmartEmbeddableLib2 = _interopRequireDefault(_cernerSmartEmbeddableLi
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-if (window.external && typeof window.external.DiscernObjectFactory !== 'undefined') {
+window.CernerSmartEmbeddableLib = window.CernerSmartEmbeddableLib || {}; /* globals window */
+
+// eslint-disable-next-line no-unused-vars
+
+
+if (window.gon.isEmbedded) {
   _cernerSmartEmbeddableLib2.default.init();
   _cernerSmartEmbeddableLib2.default.listenForCustomFrameHeight();
 
-  window.CernerSmartEmbeddableLib = window.CernerSmartEmbeddableLib || {};
   window.CernerSmartEmbeddableLib.calcFrameHeight = _cernerSmartEmbeddableLib2.default.calcFrameHeight;
   window.CernerSmartEmbeddableLib.setFrameHeight = _cernerSmartEmbeddableLib2.default.setFrameHeight;
-} /* globals window */
-
-// eslint-disable-next-line no-unused-vars
+}
 
 /***/ }),
 /* 46 */
@@ -950,7 +952,7 @@ var CernerSmartEmbeddableLib = {
   */
   init: function init() {
     _provider2.default.init({
-      acls: ['https://embedded.cerner.com', 'https://embedded.sandboxcerner.com', 'https://embedded.devcerner.com', 'https://*.devhealtheintent.com', 'https://*.staginghealtheintent.com', 'https://*.healtheintent.com']
+      acls: ['https://embedded.cerner.com', 'https://embedded.sandboxcerner.com', 'https://embedded.devcerner.com', 'https://*.devhealtheintent.com', 'https://*.staginghealtheintent.com', 'https://*.healtheintent.com', 'http://localhost:*']
     });
   },
   /**
