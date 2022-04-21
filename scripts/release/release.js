68e7d3ffb73a4895916b03875365cac4de70f26d
Shriniket Sarkar <shriniket.sarkar@cerner.com>
2022-04-21 07:23:39 -0500

    Updated the release script to ensure it tests, updates version, rebuilds dist, tags and commits to git in a single command.

--- scripts/release/release.js
+++ scripts/release/release.js
@@ -3,7 +3,9 @@ const shell = require('shelljs');
 
 const releaseType = process.argv[2] || 'patch';
 
-if (shell.exec(`npm run build && npm version ${releaseType} -m "Released version %s" && npm publish && git push`).code !== 0) {
+// npm version triggers scripts added in package.json to test, build, commit, tag and push changes with version update to git.
+// If the tests fail when ran with preversion script entire release will fail.
+if (shell.exec(`npm version ${releaseType} -m "Tagged and released version %s" && npm publish`).code !== 0) {
   shell.exit(1);
 }
 
