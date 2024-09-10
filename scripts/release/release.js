27eacca32a020a26811852a30e3bf3805f3fb3d1
M Surya <surya.rnsit@gmail.com>
2024-09-10 15:49:11 +0530

    commenting the npm publish

--- scripts/release/release.js
+++ scripts/release/release.js
@@ -5,7 +5,7 @@ const releaseType = process.argv[2] || 'patch';
 
 // npm version triggers scripts added in package.json to test, build, commit, tag and push changes with version update to git.
 // If the tests fail when ran with preversion script entire release will fail.
-if (shell.exec(`npm version ${releaseType} -m "Tagged and released version %s" && npm publish`).code !== 0) {
+if (shell.exec(`npm version ${releaseType} -m "Tagged and released version %s"`).code !== 0) {
   shell.exit(1);
 }
 
